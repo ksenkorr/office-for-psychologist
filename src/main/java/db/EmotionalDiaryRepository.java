da8b497cac0bee8dbc79f107cc0660446f76bb59
korovina <korovina@devexperts.com>
2020-05-11 17:07:55 +0300

    Pages are working, no rest api

--- src/main/java/db/EmotionalDiaryRepository.java
+++ src/main/java/db/EmotionalDiaryRepository.java
@@ -2,13 +2,15 @@ package db;
 
 import model.EmotionalDiary;
 import model.User;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface EmotionalDiaryRepository extends PagingAndSortingRepository<EmotionalDiary, Integer> {
+public interface EmotionalDiaryRepository extends CrudRepository<EmotionalDiary, Integer> {
+        //PagingAndSortingRepository<EmotionalDiary, Integer> {
 
     List<EmotionalDiary> findEmotionalDiariesByUser(User user);
 
--- src/main/java/web/WebConfiguration.java
+++ src/main/java/web/WebConfiguration.java
@@ -4,6 +4,7 @@ package web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,7 +16,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@Import(RepositoryRestMvcConfiguration.class)
+/*@Import(RepositoryRestMvcConfiguration.class)
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
@@ -27,4 +28,17 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/scripts/*.js").addResourceLocations("/scripts/");
     }
+}*/
+public class WebConfiguration {
+    @Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver("/pages/", ".jsp");
+
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
 }
+
+
